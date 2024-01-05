2632beb8298ff0e8b3a94b4c48edecb0892b4e08
harol <harol.agudelo22@gmail.com>
2024-01-05 18:32:32 -0500

    fix

--- src/app/calendar/edit-reminder/page.tsx
+++ src/app/calendar/edit-reminder/page.tsx
@@ -1,16 +1,8 @@
 "use client";
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../../redux/store'; 
 import EditReminder from "./EditReminder";
 import CurrentDateDisplay from "../common/CurrentDateDisplay";
 import AddReminderButton from "../common/AddReminderButton";
-
 export default function Reminder() {
-  const reminderToEdit = useSelector((state: RootState) => {
-    return state.reminders.find(r => r.id === selectedReminderId);
-  });
-
   return (
     <div className="pl-6 pr-14 w-3/5">
       <div className="absolute top-[50px] w-3/5 flex items-center">
@@ -18,7 +10,7 @@ export default function Reminder() {
         <AddReminderButton href="/calendar/add-reminder" />
       </div>
       <div className="absolute top-[130px] w-[567px]">
-        {reminderToEdit && <EditReminder reminder={reminderToEdit} />}
+       
       </div>
     </div>
   );
